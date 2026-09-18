b343a3590578e7f09e35c2ce0f6bdff123a58a31
emmanuelowighoyota424-byte <emmanuelowighoyota424@gmail.com>
2026-09-18 23:12:08 +0100

    Add admin customers screen

---
+++ src/app/admin/customers/page.tsx
@@ -0,0 +1,1 @@
+import AdminResourcePage from '@/components/AdminResourcePage'; export default function Customers(){return <AdminResourcePage title="Customers" resource="customers" columns={['Email','FirstName','LastName','BnkTag','KycStatus','Status','CreatedAt']}/>}
