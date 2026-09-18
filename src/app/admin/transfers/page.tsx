8d210eaa4d38af786cc2c3917a0e8f0940dc2c46
emmanuelowighoyota424-byte <emmanuelowighoyota424@gmail.com>
2026-09-18 23:12:37 +0100

    Add admin transfers screen

---
+++ src/app/admin/transfers/page.tsx
@@ -0,0 +1,1 @@
+import AdminResourcePage from '@/components/AdminResourcePage'; export default function Transfers(){return <AdminResourcePage title="Transfers" resource="transfers" columns={['Reference','Amount','Currency','Status','SenderUserId','RecipientUserId','CreatedAt']}/>}
