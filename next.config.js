a1f388b835ac5977b6329adad2d6e9b1de174094
emmanuelowighoyota424-byte <emmanuelowighoyota424@gmail.com>
2026-09-19 03:13:57 +0100

    fix: explicitly configure webpack src alias

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['bcryptjs', 'speakeasy', 'qrcode'],
   },
   typescript: {
     ignoreBuildErrors: false,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
